a6f5b216b6a8c42ce26dbc21b94656c83d6e7543
Jiasheng Li <jsli@umd.edu>
2021-08-21 10:00:43 -0400

    First html file created

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # This is the main file #
 from flask import Flask, render_template, send_file
-
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,5 +9,5 @@ def homepage():
 #    return "Welcome to the accessible web development environment."
     return send_file("static/home.html")
 
-#if __name__ == "__main__":
- #   app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
